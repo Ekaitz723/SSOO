73ffdb637edc9a055d64da7e2fa8c8a8f4bf459b
Ekaitz723 <52826804+Ekaitz723@users.noreply.github.com>
2021-01-10 23:06:08 +0100

    Actualizar to

--- SistemaDeFicheros/simul_ext_esqueleto.c
+++ SistemaDeFicheros/simul_ext_esqueleto.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include "cabeceras.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "simul_ext_func.c"
 
 #define LONGITUD_COMANDO 100
 
 
 void Printbytemaps(EXT_BYTE_MAPS *ext_bytemaps);
 
-int ComprobarComando(char *strcomando, char *orden, char *argumento1, char *argumento2);
+int ComprobarComando(char *strcomando, char datosComando[][LONGITUD_COMANDO]);
 
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *psup);
 
@@ -21,7 +21,7 @@ int Renombrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
               char *nombreantiguo, char *nombrenuevo);
 
 int Imprimir(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, 
-             EXT_DATOS *memdatos, char *nombre)
+             EXT_DATOS *memdatos, char *nombre);
 
 int Borrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
            EXT_BYTE_MAPS *ext_bytemaps, EXT_SIMPLE_SUPERBLOCK *ext_superblock,
@@ -41,64 +41,104 @@ void GrabarDatos(EXT_DATOS *memdatos, FILE *fich);
 
 //*algo que devuelva el inodo a ocupar
 
+int main() {
+	
+//	String pato = string(stdin);
+	
+	
+	char comando[LONGITUD_COMANDO];
+	char datosComando[DATOSCOMANDO][LONGITUD_COMANDO];
+	
+//	char orden[LONGITUD_COMANDO];
+//	char argumento1[LONGITUD_COMANDO];
+//	char argumento2[LONGITUD_COMANDO];
 
-int main()
-{
-	 char *comando[LONGITUD_COMANDO];
-	 char *orden[LONGITUD_COMANDO];
-	 char *argumento1[LONGITUD_COMANDO];
-	 char *argumento2[LONGITUD_COMANDO];
-	 
-	 int i,j;
-	 unsigned long int m;
-     EXT_SIMPLE_SUPERBLOCK ext_superblock;
-     EXT_BYTE_MAPS ext_bytemaps;
-     EXT_BLQ_INODOS ext_blq_inodos;
-     EXT_ENTRADA_DIR directorio[MAX_FICHEROS];
-     EXT_DATOS memdatos[MAX_BLOQUES_DATOS];
-     EXT_DATOS datosfich[MAX_BLOQUES_PARTICION];
-     int entradadir;
-     int grabardatos;
-     FILE *fent;
+	
+	printf("∫∫%i∫∫",ComprobarComando(zaborra = string(stdin),datosComando));
+	printf("\n\n∫%s %s %s∫",datosComando[0], datosComando[1], datosComando[2]);
+	free(zaborra);
+	
+	
+	return 0;
+	
+	/* 
+	int i,j;
+	unsigned long int m;
+    EXT_SIMPLE_SUPERBLOCK ext_superblock;
+    EXT_BYTE_MAPS ext_bytemaps;
+    EXT_BLQ_INODOS ext_blq_inodos;
+    EXT_ENTRADA_DIR directorio[MAX_FICHEROS];
+    EXT_DATOS memdatos[MAX_BLOQUES_DATOS];
+    EXT_DATOS datosfich[MAX_BLOQUES_PARTICION];
+    int entradadir;
+    int grabardatos;
+    FILE *fent;
      
-     // Lectura del fichero completo de una sola vez
-     ...
+    // Lectura del fichero completo de una sola vez
+    ...
      
-     fent = fopen("particion.bin","r+b");
-     fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);    
+    fent = fopen("particion.bin","r+b");
+    fread(&datosfich, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, fent);    
      
      
-     memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
-     memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
-     memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&datosfich[1], SIZE_BLOQUE);
-     memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&datosfich[2], SIZE_BLOQUE);
-     memcpy(&memdatos,(EXT_DATOS *)&datosfich[4],MAX_BLOQUES_DATOS*SIZE_BLOQUE);
+    memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&datosfich[0], SIZE_BLOQUE);
+    memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
+    memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&datosfich[1], SIZE_BLOQUE);
+    memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&datosfich[2], SIZE_BLOQUE);
+    memcpy(&memdatos,(EXT_DATOS *)&datosfich[4],MAX_BLOQUES_DATOS*SIZE_BLOQUE);
      
-     // Buce de tratamiento de comandos
-     for (;;){
-		 do {
-		 printf (">> ");
-		 fflush(stdin);
-		 fgets(comando, LONGITUD_COMANDO, stdin);
-		 } while (ComprobarComando(comando,orden,argumento1,argumento2) !=0);
-	     if (strcmp(orden,"dir")==0) {
+    // Buce de tratamiento de comandos
+    for (;;){
+		do {
+		printf (">> ");
+		fflush(stdin);
+		fgets(comando, LONGITUD_COMANDO, stdin);
+		} while (ComprobarComando(comando,datosComando));
+	    if (!strcmp(orden,"dir")) {
             Directorio(&directorio,&ext_blq_inodos);
             continue;
-            }
-         ...
-         // Escritura de metadatos en comandos rename, remove, copy     
-         Grabarinodosydirectorio(&directorio,&ext_blq_inodos,fent);
-         GrabarByteMaps(&ext_bytemaps,fent);
-         GrabarSuperBloque(&ext_superblock,fent);
-         if (grabardatos)
-           GrabarDatos(&memdatos,fent);
-         grabardatos = 0;
-         //Si el comando es salir se habr√°n escrito todos los metadatos
-         //faltan los datos y cerrar
-         if (strcmp(orden,"salir")==0){
-            GrabarDatos(&memdatos,fent);
+    		}
+        ...
+        // Escritura de metadatos en comandos rename, remove, copy     
+        Grabarinodosydirectorio(&directorio,&ext_blq_inodos,fent);
+        GrabarByteMaps(&ext_bytemaps,fent);
+        GrabarSuperBloque(&ext_superblock,fent);
+        if (grabardatos)
+        	GrabarDatos(&memdatos,fent);
+        grabardatos = 0;
+        //Si el comando es salir se habr√°n escrito todos los metadatos
+        //faltan los datos y cerrar
+        if (strcmp(orden,"salir")==0){
+        	GrabarDatos(&memdatos,fent);
             fclose(fent);
             return 0;
-         }
-     }
+        }
+    }*/
+}
+
+
+
+int ComprobarComando(char *strcomando, char datosComando[][LONGITUD_COMANDO]) {
+	if (!strcmp(strcomando, "\n")){
+		return 1;
+	}
+	
+	int i = 0;
+	String partes;
+	
+	//Se crea un array de char para guardar una copia de strcomando.
+	char completo[strlen(strcomando)+1];
+	
+	//Se vuelca strcomando en completo. Se hace con memcpy para copiar tambien el fin de cadena.
+	memcpy(completo, strcomando, strlen(strcomando)+1);
+	
+	//Se separa por partes:
+	partes = strtok(completo, " ");
+	
+	
+	do {
+		strcpy(datosComando[i++], partes);
+	} while(partes = strtok(NULL, " "));
+	
+	return 0;
 }
---
+++ SistemaDeFicheros/simul_ext_func.c
@@ -0,0 +1,44 @@
+#include "cabeceras.h"
+
+char* string(FILE *f) {
+
+	//En cuanto sumamos el realloc
+	char aumento=AUMCONSTR;
+
+	//Guarda la cadena que se devolvera.
+	char* str;
+	str = (char*)malloc(sizeof(char)*aumento);	
+	if (!str) {
+		nofufa("\nError al leer");
+	}
+	
+	//Guarda el tamaino de la cadena.
+	char tam=1;
+
+	//Guarda temporalmente cada caracter.
+	char tmp = fgetc(f);
+	
+	//Guarda cada char en str mientras no encuentre un salto de linea o EOF.
+	while( (tmp!='\n') && !feof(f)) {
+		str[tam-1]=tmp;
+		tam++;
+		if( !(tam%aumento) ) {
+			str = (char*)realloc(str,tam+aumento);
+			
+			//Si hubo algun problema con el realloc, saldra.
+			if(!str) {
+				nofufa("\nError en el realloc");
+			}
+		}
+		tmp=getc(f);
+	}
+	str[tam-1]='\0';
+	
+	return str;
+}
+
+void nofufa(char* mensaje) {
+	printf("\n");
+	printf(mensaje);
+	exit(-1);
+}
